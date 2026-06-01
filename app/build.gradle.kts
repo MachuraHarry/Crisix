a98ea5ebd37e7786c3b21348647d58132de12d19
harrymachura6-coder <harrymachura6@gmail.com>
2026-06-01 14:37:21 +0200

    Bump version to 1.4 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.messenger.crisix"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
